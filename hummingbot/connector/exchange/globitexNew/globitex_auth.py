0eb7298e1e305b47a695a1ef39a084a1dc5ab1d8
Carlos Gonçalves <carlos.goncalves@outsystems.com>
2021-04-22 22:57:37 +0100

    init request auth: headers fields

--- hummingbot/connector/exchange/globitexNew/globitex_auth.py
+++ hummingbot/connector/exchange/globitexNew/globitex_auth.py
@@ -3,56 +3,47 @@ import hashlib
 from typing import Dict, Any
 
 
-class CryptoComAuth():
+class GlobitexAuth:
     """
-    Auth class required by crypto.com API
-    Learn more at https://exchange-docs.crypto.com/#digital-signature
+    Auth class required by Globitex API
+    Learn more at https://globitex.com/api/
     """
+
     def __init__(self, api_key: str, secret_key: str):
         self.api_key = api_key
         self.secret_key = secret_key
 
-    def generate_auth_dict(
-        self,
-        path_url: str,
-        request_id: int,
-        nonce: int,
-        data: Dict[str, Any] = None
-    ):
+    def generate_auth_dict(self, path_url: str, request_id: int, nonce: int, data: Dict[str, Any] = None):
         """
         Generates authentication signature and return it in a dictionary along with other inputs
         :return: a dictionary of request info including the request signature
         """
 
         data = data or {}
-        data['method'] = path_url
-        data.update({'nonce': nonce, 'api_key': self.api_key, 'id': request_id})
+        data["method"] = path_url
+        data.update({"nonce": nonce, "api_key": self.api_key, "id": request_id})
 
-        data_params = data.get('params', {})
+        data_params = data.get("params", {})
         if not data_params:
-            data['params'] = {}
-        params = ''.join(
-            f'{key}{data_params[key]}'
-            for key in sorted(data_params)
-        )
+            data["params"] = {}
+        params = "".join(f"{key}{data_params[key]}" for key in sorted(data_params))
 
-        payload = f"{path_url}{data['id']}" \
-            f"{self.api_key}{params}{data['nonce']}"
+        payload = f"{path_url}{data['id']}" f"{self.api_key}{params}{data['nonce']}"
 
-        data['sig'] = hmac.new(
-            self.secret_key.encode('utf-8'),
-            payload.encode('utf-8'),
-            hashlib.sha256
-        ).hexdigest()
+        data["sig"] = hmac.new(self.secret_key.encode("utf-8"), payload.encode("utf-8"), hashlib.sha256).hexdigest()
 
         return data
 
-    def get_headers(self) -> Dict[str, Any]:
+    def get_headers(self, api_key: str, nonce: int, signed_message: str) -> Dict[str, Any]:
         """
-        Generates authentication headers required by crypto.com
+        Generates authentication headers required by Globitex
         :return: a dictionary of auth headers
         """
 
         return {
-            "Content-Type": 'application/json',
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "X-API-Key": api_key,
+            "X-Nonce": nonce,
+            "X-Signature": signed_message,
         }
