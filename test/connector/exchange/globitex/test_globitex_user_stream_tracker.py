3817c77a7d4abba2472c9bfa8308ac42e7c25fb6
Carlos Gonçalves <carlos.goncalves@outsystems.com>
2021-05-10 22:17:31 +0100

    start GlobitexUserStreamTrackerUnitTest

--- test/connector/exchange/globitex/test_globitex_user_stream_tracker.py
+++ test/connector/exchange/globitex/test_globitex_user_stream_tracker.py
@@ -7,24 +7,25 @@ import unittest
 import conf
 
 from os.path import join, realpath
-from hummingbot.connector.exchange.crypto_com.crypto_com_user_stream_tracker import CryptoComUserStreamTracker
-from hummingbot.connector.exchange.crypto_com.crypto_com_auth import CryptoComAuth
+from hummingbot.connector.exchange.globitex.globitex_user_stream_tracker import GlobitexUserStreamTracker
+from hummingbot.connector.exchange.globitex.globitex_auth import GlobitexAuth
 from hummingbot.core.utils.async_utils import safe_ensure_future
 
 sys.path.insert(0, realpath(join(__file__, "../../../")))
 
 
-class CryptoComUserStreamTrackerUnitTest(unittest.TestCase):
-    api_key = conf.crypto_com_api_key
-    api_secret = conf.crypto_com_secret_key
+class GlobitexUserStreamTrackerUnitTest(unittest.TestCase):
+    api_key = conf.globitex_api_key
+    api_secret = conf.globitex_api_secret
 
     @classmethod
     def setUpClass(cls):
         cls.ev_loop: asyncio.BaseEventLoop = asyncio.get_event_loop()
-        cls.crypto_com_auth = CryptoComAuth(cls.api_key, cls.api_secret)
+        cls.globitex_auth = GlobitexAuth(cls.api_key, cls.api_secret)
         cls.trading_pairs = ["BTC-USDT"]
-        cls.user_stream_tracker: CryptoComUserStreamTracker = CryptoComUserStreamTracker(
-            crypto_com_auth=cls.crypto_com_auth, trading_pairs=cls.trading_pairs)
+        cls.user_stream_tracker: GlobitexUserStreamTracker = GlobitexUserStreamTracker(
+            globitex_auth=cls.globitex_auth, trading_pairs=cls.trading_pairs
+        )
         cls.user_stream_tracker_task: asyncio.Task = safe_ensure_future(cls.user_stream_tracker.start())
 
     def test_user_stream(self):
